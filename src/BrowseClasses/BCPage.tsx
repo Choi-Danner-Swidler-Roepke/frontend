6ae94e53b12cbf99a33b28231269c3a5fb437fb4
Jacob Danner <jacob.danner@drake.edu>
2023-02-15 14:12:55 -0600

    bare minimum table displaying classes with hardcoded data

--- src/BrowseClasses/BCPage.tsx
+++ src/BrowseClasses/BCPage.tsx
@@ -1,7 +1,14 @@
+import { classes } from "./fakeData"
+import { Table } from "./Table"
+
 export const BCPage: React.FC = () => {
+
+    console.log(classes)
+    // classes will be fetched here and passed to Table
+
     return (
-        <div>
-            Browse Classses Page
+        <div className="bg-slate-300 flex justify-center items-center h-full w-full">
+            <Table data={classes} />
         </div>
     )
 }
--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom"
 
 export const Navbar: React.FC = () => {
     return (
-        <nav id="navbar" className="w-full bg-neutral-200 shadow-md" >
+        <nav id="navbar" className="w-full bg-neutral-200 shadow-md absolute" >
             <ul className="flex justify-around">
                 <li> <Link to={"/"} className="text-xl opacity-60 font-semibold hover:opacity-100">HOME</Link> </li>
                 <li> <Link to={"/browse-classes"} className="text-xl opacity-60 font-semibold hover:opacity-100">BROWSE CLASSES</Link> </li>
