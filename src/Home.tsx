dcf6d897e47cdde54aab1a0faf18b680098193d6
lswidler37 <lucas.swidler@drake.edu>
2023-03-05 15:58:23 -0600

    Home page changes

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,9 +1,14 @@
 export const Home: React.FC = () => {
     console.log('here')
     return (
-        <div className="fixed left-72 bg-blue-700 w-200 h-200">
-            Home
+        <div>
+            <img className ="static h-fit w-fit" src="./src/assets/home_screen.jpg"></img>
+            <h1 class="absolute text-5xl font-serif text-blue-400 border-2 border-black top-1/3 left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white">
+                Registration Application
+                <h2 class="relative text-3xl text-black top">
+                    Register for your classes <a class="underline decoration-indigo-500">easier</a>
+                </h2>
+            </h1>
         </div>
-        
     )
 }
